4b379257e29b228fa4b8fc773e273759d72837c0
Juanjo.figue97 <juanjo.figue97@gmail.com>
2022-09-11 18:21:55 -0500

    Opciones de video

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// CONTROL UBICACION DE VIDEOS 
 let listVideo = document.querySelectorAll('.video-list .vid');
 let mainVideo = document.querySelector('.main-video video')
 let title = document.querySelector('.main-video .title')
@@ -22,7 +23,9 @@ listVideo.forEach(video => {
 function liveViews(response) {
     document.getElementById('visits').innerText = response.value;
 }
+// FIN DE CONTROL UBICACION DE VIDEOS
 
+//CONTADOR DE VISITAS
 var counterContainer = document.querySelector(".website-counter");
 var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
@@ -45,36 +48,15 @@ resetButton.addEventListener("click", () => {
 });
 
 
-function get_viewrs_ip(json){
-    viwers_ip = json.ip;
-    count_view(viwers_ip);
-}
-
-function count_view(viwers_ip){
-    var views;
-    var ip_to_string = viwers_ip.toString();
-
-    for(var i, i = 0; i < ip_to_string.length; i++){
-        ip_to_string =ip_to_string.replace(".","-")
-    }
-    firebase.database().ref().child("pages_views/" + ip_to_string).set({
-        viwers_ip: viwers_ip
-    });
-
-    firebase.database().ref().child("page_views").once("value", function(snapshot){
-        views = snapshot.numChildren();
-        // document.getElementById("view_count_text").innerHTML = 
-    })
-}
-
-// Controls
+// CONTROLES DE VIDEO
 
 var video = document.querySelector('#video-principal');
 var juice = document.querySelector('.orange-juice');
 var btn = document.querySelector('#play-pause');
-var icon = document.getElementById('icon')
+var icon = document.getElementById('icon');
+var volume = document.getElementById('volumen-bar');
 
-// fa-solid fa-pause
+
 function togglePlayPause(){
     if(video.paused){
         btn.className = 'pause';
@@ -107,20 +89,100 @@ video.addEventListener('timeupdate', function(){
     }
 })
 
+volume.addEventListener('change', function(e){
+    video.volume = e.currentTarget.value /100;
+    console.log(video.volume,e)
+    console.log(video.currentTime,video.duration);
+})
 
-
-// var startDate;
-// function PlayVideo(video) {
-//     console.log(video + " is playing");
-//     startDate = new Date();
+// vol.onclick = function(){
+//     sounds()
 // }
 
-// function PlayVideoCount() {
-//     console.log("stooped video");
-//     var diff = (new Date() - startDate);
+// MENU DESPLEGABLE
 
-//     var minutes = Math.floor((diff / 1000) / 60);
-//     var second = Math.floor((diff / 1000));
-//     console.log("watched video " + minutes + " minutes");
-//     console.log("watched video " + second + " seconds");
-// }
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("prin").style.marginLeft = "250px";
+    document.getElementById("menu-prin").style.paddingLeft = "0";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("prin").style.marginLeft = "0";
+    document.getElementById("menu-prin").style.paddingLeft = "70px";
+}
+
+// FIN DE MENU DESPLEGABLE
+
+var timeStarted = -1;
+var timePlayed = 0;
+var duration = 0;
+// If video metadata is laoded get duration
+if(video.readyState > 0)
+  getDuration.call(video);
+//If metadata not loaded, use event to get it
+else
+{
+  video.addEventListener('loadedmetadata', getDuration);
+}
+// remember time user started the video
+function videoStartedPlaying() {
+  timeStarted = new Date().getTime()/1000;
+}
+function videoStoppedPlaying(event) {
+  // Start time less then zero means stop event was fired vidout start event
+  if(timeStarted>0) {
+    var playedFor = new Date().getTime()/1000 - timeStarted;
+    timeStarted = -1;
+    // add the new number of seconds played
+    timePlayed+=playedFor;
+  }
+  document.getElementById("played").innerHTML = Math.round(timePlayed)+"";
+  // Count as complete only if end of video was reached
+  if(timePlayed>=duration && event.type=="ended") {
+    document.getElementById("status").className="complete";
+  }
+}
+
+function getDuration() {
+  duration = video.duration;
+  document.getElementById("duration").appendChild(new Text(Math.round(duration)+""));
+  console.log("Duration: ", duration);
+}
+
+video.addEventListener("play", videoStartedPlaying);
+video.addEventListener("playing", videoStartedPlaying);
+video.addEventListener("ended", videoStoppedPlaying);
+video.addEventListener("pause", videoStoppedPlaying);
+
+let ip = document.getElementById('gfg');
+console.log(ip);
+let a = ip.textContent;
+console.log(a)
+var dict = {"1" : ip};
+var dictstring = JSON.stringify(dict);
+console.log(dictstring);
+
+let ip_Adress = 'h';
+console.log(ip_Adress)
+
+import("https://api.ipify.org?format=jsonp&callback=getIP");
+function getIP(json) { 
+    console.log((`Your IP Address is ${json.ip}`))
+    ip_Adress += json.ip;
+}
+
+function json(url) {
+    return fetch(url).then(res => res.json());
+}
+
+let apiKey = 'cc49dc0d3bf75fcaa4f502e4f42e3c2b95d504972ad1dcc746128088';
+json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+    console.log(data.ip);
+    console.log(data.city);
+    console.log(data.country_code);
+    // so many more properties
+});
+
+console.log(video.currentTime)
