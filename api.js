4ea4ee41b6f0c2f6bc7bd271a9bd4594e53e6f89
juanjo.figue97 <juanjo.figue97@gmail.com>
2022-09-14 15:59:16 -0500

    Datos de los Usuarios

--- api.js
+++ api.js
@@ -24,6 +24,7 @@ function mostrar(){
         .then(agencia =>{
             localStorage.Agencia = agencia;
             swal("Datos","Nombre del usuatio: " + name + "\nAgencia: " + agencia, "success");
+            adddGS();
         })
     })
 }
